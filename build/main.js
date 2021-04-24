7ac59752ce3b69c7067f011834a952638e31f9cb
HARSHITHV25 <harshithv25@gmail.com>
2021-04-24 21:39:53 +0530

    first commit

---
+++ build/main.js
@@ -0,0 +1,137 @@
+import "./style.css";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWrenwacoju8VFy2a9OndHBXTf73x_qe8",
+  authDomain: "webrtc-b8b16.firebaseapp.com",
+  projectId: "webrtc-b8b16",
+  storageBucket: "webrtc-b8b16.appspot.com",
+  messagingSenderId: "101612840829",
+  appId: "1:101612840829:web:9da9b09c550fcab8324e14",
+  measurementId: "G-6NS2GH3YHJ",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const firestore = firebase.firestore();
+
+const servers = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.1.google.com:19302", "stun:stun2.1.google.com:19302"],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
+
+let pc = new RTCPeerConnection(servers);
+let localStream = null;
+let remoteStream = null;
+
+const webcamButton = document.getElementById("webcamButton");
+const webcamVideo = document.getElementById("webcamVideo");
+const callButton = document.getElementById("callButton");
+const callInput = document.getElementById("callInput");
+const answerButton = document.getElementById("answerButton");
+const remoteVideo = document.getElementById("remoteVideo");
+const hangupButton = document.getElementById("hangupButton");
+
+webcamButton.onclick = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+  });
+  remoteStream = new MediaStream();
+  localStream.getTracks().forEach((track) => {
+    pc.addTrack(track, localStream);
+  });
+  pc.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+  webcamVideo.srcObject = localStream;
+  remoteVideo.srcObject = remoteStream;
+
+  callButton.disabled = false;
+  answerButton.disabled = false;
+  webcamButton.disabled = true;
+};
+
+callButton.onclick = async () => {
+  const callDoc = firestore.collection("calls").doc();
+  const offerCandidates = callDoc.collection("offerCandidates");
+  const answerCandidates = callDoc.collection("answerCandidates");
+  callInput.value = callDoc.id;
+
+  pc.onicecandidate = (event) => {
+    event.candidate && offerCandidates.add(event.candidate.toJSON());
+  };
+
+  const offerDescription = await pc.createOffer();
+  await pc.setLocalDescription(offerDescription);
+  const offer = {
+    sdp: offerDescription.sdp,
+    type: offerDescription.type,
+  };
+  await callDoc.set({ offer });
+
+  callDoc.onSnapshot((snapshot) => {
+    const data = snapshot.data();
+    if (!pc.currentRemoteDescription && data?.answer) {
+      const answerDescription = new RTCSessionDescription(data.answer);
+      pc.setRemoteDescription(answerDescription);
+    }
+  });
+
+  answerCandidates.onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      if (change.type === "added") {
+        const candidate = new RTCIceCandidate(change.doc.data());
+        pc.addIceCandidate(candidate);
+      }
+    });
+  });
+
+  hangupButton.disabled = false;
+};
+
+answerButton.onclick = async () => {
+  const callId = callInput.value;
+  const callDoc = firestore.collection("calls").doc(callId);
+  const answerCandidates = callDoc.collection("answerCandidates");
+  const offerCandidates = callDoc.collection("offerCandidates");
+
+  pc.onicecandidate = (event) => {
+    event.candidate && answerCandidates.add(event.candidate.toJSON());
+  };
+
+  const callData = (await callDoc.get()).data();
+
+  const offerDescription = callData.offer;
+  await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+
+  const answerDescription = await pc.createAnswer();
+  await pc.setLocalDescription(answerDescription);
+
+  const answer = {
+    type: answerDescription.type,
+    sdp: answerDescription.sdp,
+  };
+
+  await callDoc.update({ answer });
+
+  offerCandidates.onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      console.log(change);
+      if (change.type === "added") {
+        let data = change.doc.data();
+        pc.addIceCandidate(new RTCIceCandidate(data));
+      }
+    });
+  });
+};
---
+++ firebase.js
@@ -0,0 +1,19 @@
+// {
+//     "hosting": {
+//       "site": "webrtc-17",
+//       "public": "public",
+//       ...
+//     }
+//   }
+// firebase deploy --only hosting:webrtc-17
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAWrenwacoju8VFy2a9OndHBXTf73x_qe8",
+  authDomain: "webrtc-b8b16.firebaseapp.com",
+  projectId: "webrtc-b8b16",
+  storageBucket: "webrtc-b8b16.appspot.com",
+  messagingSenderId: "101612840829",
+  appId: "1:101612840829:web:9da9b09c550fcab8324e14",
+  measurementId: "G-6NS2GH3YHJ",
+};
